20fe5747fef61fcfc99aba1a7959f7e23f1974ab
Alexis Chavez <achavez@michelada.io>
2018-04-24 15:33:30 -0500

    devise

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class Buyer < ApplicationRecord
-  validates :control_number, presence: true, length: { is: 8 }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates :email, uniqueness: true
+  validates :control_number, presence: true, length: { is: 8 }, uniqueness: true
   validates :name, presence: true
-  validates :phone, presence: true
+  validates :phone, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_23_214916) do
+ActiveRecord::Schema.define(version: 2018_04_23_232612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,19 @@ ActiveRecord::Schema.define(version: 2018_04_23_214916) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.index ["control_number"], name: "index_buyers_on_control_number", unique: true
+    t.index ["email"], name: "index_buyers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_buyers_on_reset_password_token", unique: true
   end
 
   create_table "stores", force: :cascade do |t|
